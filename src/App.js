594f89fbbb24b70894c67f7fd5da60d7ee29ac63
yukyo <mrym12000@gmail.com>
2020-07-04 10:46:40 +0900

    change welcome message

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <h1 classname="App-title">hello world</h1>
+	<p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
